7889ee0919394f67d5cddebbe44078698ab9a2ab
André Corrêa <andre.murillo.correa@gmail.com>
2021-09-01 13:33:42 -0300

    create the signin page

---
+++ src/components/ButtonIcon/styles.ts
@@ -0,0 +1,31 @@
+import { StyleSheet } from 'react-native';
+import { theme } from '../../global/styles/theme';
+
+export const styles = StyleSheet.create({
+    container: {
+        backgroundColor: theme.colors.primary,
+        width: '100%',
+        height: 56,
+        borderRadius: 8,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    iconWrapper: { 
+        width: 56,
+        height: 56,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRightWidth: 1,
+        borderRightColor: theme.colors.line
+    },
+    icon: {
+        width: 24,
+        height: 17
+    },
+    title: {
+        color: theme.colors.heading,
+        flex: 1,
+        fontSize: 15,
+        paddingHorizontal: 40
+    }
+});
